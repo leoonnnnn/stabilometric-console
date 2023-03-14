d06ca34ffdbd276e8fe8a339be4fee69f99bb28a
leoonnnnn <39069236+leoonnnnn@users.noreply.github.com>
2023-03-13 18:28:11 -0700

    Added Joystick Control Code

--- Stabilometric Console/Assets/Scripts/PlayerMovement.cs
+++ Stabilometric Console/Assets/Scripts/PlayerMovement.cs
@@ -47,10 +47,6 @@
 
 
 // controlling using joystick + arduino
-// code hereererer
-
-
-// controlling using pressure sensors + arduino
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -69,7 +65,7 @@ public class PlayerMovement : MonoBehaviour {
 
     public float speedIncreasePerPoint = 0.1f;
 
-    SerialPort data_stream = new SerialPort("COM5", 9600);
+    SerialPort data_stream = new SerialPort("COM4", 9600);
     public string receivedstring;
     public GameObject test_data;
     //public Rigidbody rb;
@@ -102,7 +98,7 @@ public class PlayerMovement : MonoBehaviour {
         string[] datas = receivedstring.Split(',');  
         Debug.Log(datas[0]);
 
-        horizontalInput = float.Parse(datas[0])/2;
+        horizontalInput = float.Parse(datas[1])/-1000;  //hold the joystick with wires facing forward
 
         if (transform.position.y < -5) {
             Die();
@@ -121,3 +117,76 @@ public class PlayerMovement : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
+
+
+// controlling using pressure sensors + arduino
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+
+// using System.Collections;
+// using System.IO.Ports;
+
+// public class PlayerMovement : MonoBehaviour {
+
+//     bool alive = true;
+
+//     public float speed = 5;
+//     [SerializeField] Rigidbody rb;
+
+//     float horizontalInput;
+//     [SerializeField] float horizontalMultiplier = 2;
+
+//     public float speedIncreasePerPoint = 0.1f;
+
+//     SerialPort data_stream = new SerialPort("COM5", 9600);
+//     public string receivedstring;
+//     public GameObject test_data;
+//     //public Rigidbody rb;
+//     //rb = getcomponent<Rigidbody>();
+//     public float sensitivity = 0.01f;
+
+//     public string[] datas;
+
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();   //added this line
+//         Debug.Log("hello world");
+//         data_stream.Open();
+//         Debug.Log("hello world again");
+//     }
+
+
+//     private void FixedUpdate ()
+//     {
+//         if (!alive) return;
+
+//         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+//         Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+//         rb.MovePosition(rb.position + forwardMove + horizontalMove);        
+//     }
+
+//     private void Update () {
+//         receivedstring = data_stream.ReadLine();
+//         //Debug.Log(receivedstring);
+//         string[] datas = receivedstring.Split(',');  
+//         Debug.Log(datas[0]);
+
+//         horizontalInput = float.Parse(datas[0])/2;
+
+//         if (transform.position.y < -5) {
+//             Die();
+//         }
+// 	}
+
+//     public void Die ()
+//     {
+//         alive = false;
+//         // Restart the game
+//         Invoke("Restart", 2);
+//     }
+
+//     void Restart ()
+//     {
+//         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+//     }
+// }
